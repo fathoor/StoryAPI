ad8bde7c570d2238b53b7866376c0496b71b3bb7
fathoor <fathurrahman.205026@mhs.its.ac.id>
2023-04-24 20:13:36 +0700

    `update` dependency injection

---
+++ app/src/main/java/com/fathoor/storyapi/model/di/Injection.kt
@@ -0,0 +1,17 @@
+package com.fathoor.storyapi.model.di
+
+import android.content.Context
+import com.fathoor.storyapi.model.local.preference.UserPreference
+import com.fathoor.storyapi.model.local.preference.dataStore
+import com.fathoor.storyapi.model.remote.retrofit.ApiConfig
+import com.fathoor.storyapi.model.repository.UserRepository
+import com.fathoor.storyapi.view.helper.AppExecutor
+
+object Injection {
+    fun provideUserRepository(context: Context): UserRepository {
+        val apiService = ApiConfig.getApiService()
+        val userPreference = UserPreference.getInstance(context.dataStore)
+        val appExecutor = AppExecutor()
+        return UserRepository.getInstance(apiService, userPreference, appExecutor)
+    }
+}
